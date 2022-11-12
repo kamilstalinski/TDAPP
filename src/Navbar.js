b6e5cc2b6b667cd2c5a1eed569c76b4ee6c4c3f3
kamilstalinski <kamstal95@gmail.com>
2022-11-12 21:40:53 +0100

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='icon-wrapper'>
+        <h2>Icon</h2>
+        <h2>Icon</h2>
+        <h2>Icon</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
